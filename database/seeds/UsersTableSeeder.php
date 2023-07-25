d1fc83b2e55681e24516fb71c498d82af27d24af
shi0rin <shiori1641276@gmail.com>
2023-07-25 23:37:05 +0900

    やり直し、シードで初期ユーザー登録まで完了

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'username' => '一郎',
+            'mail' => 'abc@atlas',
+            'password' => bcrypt('12345'),
+
+        ]);
+        DB::table('users')->insert([
+            'username' => '二郎',
+            'mail' => 'def@atlas',
+            'password' => bcrypt('12345'),
+
+        ]);
+        DB::table('users')->insert([
+            'username' => '三郎',
+            'mail' => 'ghi@atlas',
+            'password' => bcrypt('12345'),
+
+        ]);
+    }
+}
